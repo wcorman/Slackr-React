0820a30532b701ba3fac1d6afc1b32671d663b2b
Jason <jzntam@gmail.com>
2018-04-09 22:48:22 -0700

    Continue cleanup, remove unused code and reformat Sliders component

--- src/components/Sliders.js
+++ src/components/Sliders.js
@@ -12,21 +12,20 @@ const defaultSliderState = {
 
 class Sliders extends Component {
   constructor(props) {
-
     super(props)
     this.state = defaultSliderState
 
     this.handleSubmit = this.handleSubmit.bind(this);
-
-    // const {onSubmit = () => {}} = props
-    // this.onSubmit = props.onSubmit
+    this.handleSliderChange = this.handleSliderChange.bind(this);
   }
 
-  handleSliderChange = (value, name) => {
-    this.setState({[name]: value})
+  handleSliderChange(value, name) {
+    this.setState({
+      [name]: value
+    })
   }
 
-  handleSubmit = (e) => {
+  handleSubmit(e) {
     e.preventDefault();
 
     const data = new FormData(e.target);
@@ -44,65 +43,91 @@ class Sliders extends Component {
   }
 
   render() {
-    const {prodValue, unprodValue, sleepValue, happyValue} = this.state
+    const { prodValue, unprodValue, sleepValue, happyValue } = this.state
     const horizontalLabels = {
       0: 'Low',
-      // 50: 'Medium',
       100: 'High'
     }
 
     const formatPc = p => p + '%'
 
-    return (<div style={{
-        width: '27vw',
-        minWidth: '240px'
-      }} className='slider custom-labels'>
-      <div style={{
-          marginBottom: '20px'
-        }}>
-        <h2 className='headings'>How was your day?</h2>
-      </div>
-
-        <div style={{
-            marginBottom: '10px'
-          }}>
-          <p style={{
-              fontSize: '19px',
-              color: 'tomato'
-            }}>Procrastination</p>
-          <Slider className='unprodSelect' min={0} max={100} value={unprodValue} labels={horizontalLabels} format={formatPc} handleLabel={unprodValue} onChange={(value) => this.handleSliderChange(value, 'unprodValue')}/>
+    return (
+      <div style={{ width: "27vw", minWidth: "240px" }} className="slider custom-labels">
+        <div style={{ marginBottom: "20px" }}>
+          <h2 className="headings">How was your day?</h2>
         </div>
-        <p style={{
-            fontSize: '19px',
-            color: 'palegreen'
-          }}>Healthy Meals</p>
-        <Slider className='prodSelect' min={0} max={100} value={prodValue} labels={horizontalLabels} format={formatPc} handleLabel={prodValue} onChange={(value) => this.handleSliderChange(value, 'prodValue')}/>
-      <div style={{
-          marginBottom: '10px'
-        }}>
-        <p style={{
-            fontSize: '19px',
-            color: '#15fbff'
-          }}>Sleep Quality</p>
-        <Slider className='sleepSelect' min={0} max={100} value={sleepValue} labels={horizontalLabels} format={formatPc} handleLabel={sleepValue} onChange={(value) => this.handleSliderChange(value, 'sleepValue')}/>
-      </div>
-      <div style={{
-          marginBottom: '40px'
-        }}>
-        <p style={{
-            fontSize: '19px',
-            color: 'gold'
-          }}>Mood</p>
-        <Slider className='happySelect' min={0} max={100} value={happyValue} labels={horizontalLabels} format={formatPc} handleLabel={happyValue} onChange={(value) => this.handleSliderChange(value, 'happyValue')}/>
-      </div>
-      <MuiThemeProvider>
-        <RaisedButton label="Submit" primary={true} style={{
-            width: '100px',
-            borderRadius: '12px'
-          }} color="primary" onClick={(e) => this.handleSubmit(e)}></RaisedButton>
-      </MuiThemeProvider>
 
-    </div>)
+        <div style={{ marginBottom: "10px" }}>
+          <p style={{ fontSize: "19px", color: "tomato" }}>
+            Procrastination
+          </p>
+          <Slider
+            className="unprodSelect"
+            min={0}
+            max={100}
+            value={unprodValue}
+            labels={horizontalLabels}
+            format={formatPc}
+            handleLabel={unprodValue}
+            onChange={value => this.handleSliderChange(value, "unprodValue")}
+          />
+        </div>
+        <div style={{ marginBottom: "10px" }}>
+          <p style={{ fontSize: "19px", color: "palegreen" }}>
+            Healthy Meals
+          </p>
+          <Slider
+            className="prodSelect"
+            min={0}
+            max={100}
+            value={prodValue}
+            labels={horizontalLabels}
+            format={formatPc}
+            handleLabel={prodValue}
+            onChange={value => this.handleSliderChange(value, "prodValue")}
+          />
+        </div>
+        <div style={{ marginBottom: "10px" }}>
+          <p style={{ fontSize: "19px", color: "#15fbff" }}>
+            Sleep Quality
+          </p>
+          <Slider
+            className="sleepSelect"
+            min={0}
+            max={100}
+            value={sleepValue}
+            labels={horizontalLabels}
+            format={formatPc}
+            handleLabel={sleepValue}
+            onChange={value => this.handleSliderChange(value, "sleepValue")}
+          />
+        </div>
+        <div style={{ marginBottom: "10px" }}>
+          <p style={{ fontSize: "19px", color: "#15fbff" }}>
+            Mood
+          </p>
+          <Slider
+            className="happySelect"
+            min={0}
+            max={100}
+            value={happyValue}
+            labels={horizontalLabels}
+            format={formatPc}
+            handleLabel={happyValue}
+            onChange={value => this.handleSliderChange(value, "happyValue")}
+          />
+        </div>
+        <MuiThemeProvider>
+          <RaisedButton
+            label="Submit"
+            primary={true}
+            style={{ width: "100px", borderRadius: "12px" }}
+            color="primary"
+            onClick={e => this.handleSubmit(e)}
+          />
+        </MuiThemeProvider>
+      </div>
+    );
   }
 }
 
